d2a2a18127b426675aea23b662b5fe0701b81b41
darkrider2109 <sanjeevkumarv2.0@gmail.com>
2020-05-30 20:45:02 +0530

    add security on post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const postControllers = require('../controllers/posts_controller');
-router.post('/create', postControllers.create);
+router.post('/create', passport.checkAuthentication, postControllers.create);
 module.exports = router;
